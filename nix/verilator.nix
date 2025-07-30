7e00bf5edc8c750ae1392e28d51ad83b693f812b
Leo Moser <leomoser99@gmail.com>
2025-07-30 17:05:07 +0200

    Update verilator to 5.038 (#28)
    - Update verilator to 5.038
    - This update makes it possible to compile and simulate designs
    
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- nix/verilator.nix
+++ nix/verilator.nix
@@ -1,4 +1,4 @@
-# Copyright 2023 Efabless Corporation
+# Copyright 2025 Ciel Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 # Code adapated from nixpkgs, original license follows
 # ---
-# Copyright (c) 2003-2023 Eelco Dolstra and the Nixpkgs/NixOS contributors
+# Copyright (c) 2003-2025 Eelco Dolstra and the Nixpkgs/NixOS contributors
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -34,59 +34,119 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 {
-  clangStdenv,
+  lib,
+  stdenv,
   fetchFromGitHub,
   perl,
-  makeWrapper,
   flex,
   bison,
   python3,
   autoconf,
-  help2man,
   which,
+  help2man,
+  makeWrapper,
+  systemc,
+  git,
+  numactl,
+  coreutils,
+  gdb,
   glibcLocales,
-  lib,
-  version ? "v5.018",
+  version ? "5.038",
   rev ? null,
-  sha256 ? "sha256-f06UzNw2MQ5me03EPrVFhkwxKum/GLDzQbDNTBsJMJs=",
+  sha256 ? "sha256-uPGVE7y3zm+5ZydGjd1+/kIjW+a5u6d+YzjUSE4KnCY=",
 }:
-clangStdenv.mkDerivation {
+
+stdenv.mkDerivation rec {
   pname = "verilator";
   inherit version;
+
+  # Verilator gets the version from this environment variable
+  # if it can't do git describe while building.
+  VERILATOR_SRC_VERSION = "v${version}";
 
   src = fetchFromGitHub {
     owner = "verilator";
     repo = "verilator";
     rev =
       if rev == null
-      then version
+      then "v${version}"
       else rev;
     inherit sha256;
   };
 
   enableParallelBuilding = true;
-  buildInputs = [perl];
-  nativeBuildInputs = [makeWrapper flex bison python3 autoconf help2man];
-  nativeCheckInputs = [which];
+  buildInputs = [
+    perl
+    systemc
+    (python3.withPackages (
+      pp: with pp; [
+        distro
+      ]
+    ))
+    # ccache
+  ];
+  nativeBuildInputs = [
+    makeWrapper
+    flex
+    bison
+    autoconf
+    help2man
+    git
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    gdb
+  ];
+
+  nativeCheckInputs = [
+    which
+    coreutils
+    # cmake
+    python3
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    numactl
+  ];
+
+  doCheck = true;
+  checkTarget = "test";
 
   preConfigure = "autoconf";
 
   postPatch = ''
-    patchShebangs bin/* src/{flexfix,vlcovgen}
+    patchShebangs bin/* src/* nodist/* docs/bin/* examples/xml_py/* \
+    test_regress/{driver.py,t/*.{pl,pf}} \
+    test_regress/t/t_a1_first_cc.py \
+    test_regress/t/t_a2_first_sc.py \
+    ci/* ci/docker/run/* ci/docker/run/hooks/* ci/docker/buildenv/build.sh
+    # verilator --gdbbt uses /bin/echo to test if gdb works.
+    substituteInPlace bin/verilator --replace-fail "/bin/echo" "${coreutils}/bin/echo"
+  '';
+  # grep '^#!/' -R . | grep -v /nix/store | less
+  # (in nix-shell after patchPhase)
+
+  # This is needed to ensure that the check phase can find the verilator_bin_dbg.
+  preCheck = ''
+    export PATH=$PWD/bin:$PATH
   '';
 
-  postInstall = lib.optionalString clangStdenv.isLinux ''
+  env = {
+    SYSTEMC_INCLUDE = "${lib.getDev systemc}/include";
+    SYSTEMC_LIBDIR = "${lib.getLib systemc}/lib";
+  };
+
+  postInstall = lib.optionalString stdenv.isLinux ''
     for x in $(ls $out/bin/verilator*); do
       wrapProgram "$x" --set LOCALE_ARCHIVE "${glibcLocales}/lib/locale/locale-archive"
     done
   '';
 
-  doCheck = false;
-
   meta = with lib; {
     description = "Fast and robust (System)Verilog simulator/compiler and linter";
     homepage = "https://www.veripool.org/verilator";
-    license = with licenses; [lgpl3Only artistic2];
+    license = with licenses; [
+      lgpl3Only
+      artistic2
+    ];
     platforms = platforms.unix;
   };
 }
