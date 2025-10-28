73a67c4d664814d242e1fb93086f5895b81c87c4
Leo Moser <leomoser99@gmail.com>
2025-10-28 18:42:26 +0100

    chore: update magic, netgen (#36)
    - magic to 8.3.568
    - netgen to 1.5.305
    
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- nix/netgen.nix
+++ nix/netgen.nix
@@ -19,9 +19,9 @@
   tk,
   m4,
   python3,
-  version ? "1.5.300",
+  version ? "1.5.305",
   rev ? null,
-  sha256 ? "sha256-LM6u3ovhIwbHMWmQP/Ajde4f99mN0SHG+cuhrCAfQBs=",
+  sha256 ? "sha256-/FHE73k+Wb0q+N/0/XgrlJxpg+8+/T817Pbqfw8XmgQ=",
 }:
 clangStdenv.mkDerivation {
   pname = "netgen";
