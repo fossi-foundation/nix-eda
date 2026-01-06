13b6bc2b9c9ce8e96a0f6c983ca4635e3779fc8a
Mohamed Gaber <me@donn.website>
2026-01-06 16:52:10 +0200

    misc: optimize nix_copy_s3 for performance

--- .github/actions/nix_sign_cache_s3/nix_copy_s3.py
+++ .github/actions/nix_sign_cache_s3/nix_copy_s3.py
@@ -37,11 +37,13 @@ import os
 import re
 import json
 import argparse
+import shlex
 import sys
 import tempfile
 import subprocess
 import logging
 from typing import Any, List, Dict, Set
+from urllib.parse import urlparse
 
 ws_rx = re.compile(r"\s+")
 
@@ -76,6 +78,8 @@ def parse_narinfo(file: io.TextIOWrapper):
 def check_json_out(args: List[str], **kwargs):
     if "encoding" not in kwargs:
         kwargs["encoding"] = "utf8"
+    if os.getenv("VERBOSE", "0") == "1":
+        print(f"$ {shlex.join(args)}", file=sys.stderr)
     out_str = subprocess.check_output(args, **kwargs)
     return json.loads(out_str)
 
@@ -97,8 +101,10 @@ def main(text_args):
     args.add_argument(
         "-u",
         "--upstream-cache",
-        help="Check this upstream cache before uploading to the supplementary S3 bucket-based cache.",
+        help="Check if the paths exist in any of these upstream caches before attempting to upload to the S3 cache. If an element is the exact string 'TARGET_HTTPS', the final s3 cache is assumed accessible over HTTPS and also checked.",
         required=True,
+        nargs="+",
+        action="extend",
     )
     args.add_argument(
         "-t",
@@ -118,12 +124,24 @@ def main(text_args):
     paths_queried: Set[str] = set()
     paths_in_upstream_caches: Dict[str, str] = {}
 
+    s3_nix_url = args_parsed.to_s3_bucket
+    if "://" not in s3_nix_url:
+        s3_nix_url = f"s3://{s3_nix_url}"
+    s3_nix_url = urlparse(s3_nix_url)._replace(scheme="s3")
+
+    upstream_caches = []
+    for cache in args_parsed.upstream_cache:
+        if cache == "TARGET_HTTPS":
+            upstream_caches.append(s3_nix_url._replace(scheme="https").geturl())
+        else:
+            upstream_caches.append(cache)
+
     for i, flake_output in enumerate(args_parsed.flake_outputs):
         logging.info(
             f"Processing {flake_output} ({i + 1}/{len(args_parsed.flake_outputs)})…"
         )
 
-        # 0. List all paths that this flake output depends on.
+        # 0. List all paths that this flake output depends on, the "closure"
         try:
             closure_raw: Any = check_json_out(
                 ["nix", "path-info", "--recursive", "--json", flake_output],
@@ -135,6 +153,11 @@ def main(text_args):
                     f"Failed to get store paths for {flake_output} -- assuming broken, skipping…"
                 )
                 continue
+            elif "does not exist in the store" in e.stderr:
+                logging.warning(
+                    f"{flake_output} does not exist in the store, possibly unbuilt. Skipping…"
+                )
+                continue
             else:
                 raise e from None
 
@@ -142,47 +165,44 @@ def main(text_args):
         if closure is None:
             continue
 
-        # 1. Check which paths have already been queried upstream, and
-        # which need to be queried.
+        # 1. Check which paths have already been queried in previous attempts
+        #    to avoid hammering upstreams.
         paths_to_query: Set[str] = set()
         for path in closure:
             if path not in paths_queried:
                 paths_to_query.add(path)
-                paths_queried.add(path)
 
-        # 2. Query paths that need to be queried against upstream.
+        # 2. Query any paths that have not already been queried
         if len(paths_to_query):
             logging.info("Checking for paths upstream…")
-            upstream_cache_info_raw = check_json_out(
-                [
-                    "nix",
-                    "path-info",
-                    "--json",
-                    "--eval-store",
-                    "",
-                    "--store",
-                    args_parsed.upstream_cache,
-                    *paths_to_query,
-                ],
-                stderr=subprocess.PIPE,
-            )
-            upstream_cache_paths = paths_from_path_info(upstream_cache_info_raw)
-            if upstream_cache_paths is None:
-                continue
+            for cache in upstream_caches:
+                upstream_cache_info_raw = check_json_out(
+                    [
+                        "nix",
+                        "path-info",
+                        "--json",
+                        "--eval-store",
+                        "",
+                        "--store",
+                        cache,
+                        *paths_to_query,
+                    ],
+                    stderr=subprocess.PIPE,
+                )
+                upstream_cache_paths = paths_from_path_info(upstream_cache_info_raw)
+                if upstream_cache_paths is None:
+                    continue
 
-            # 2a. Update list of paths known to be upstream
-            #
-            #    We store which cache for the probability of supporting multiple
-            #    caches in the future. Don't count on it though.
-            paths_in_upstream_caches.update(
-                {path: args_parsed.upstream_cache for path in upstream_cache_paths}
-            )
+                paths_in_upstream_caches.update(
+                    {path: cache for path in upstream_cache_paths}
+                )
+            paths_queried |= paths_to_query
 
         # 3. Upload remaining paths from closure, if any, to our S3-based cache.
         difference = closure - set(paths_in_upstream_caches.keys())
         if len(difference):
             logging.info(
-                "One or more paths not found in upstream cache and will be uploaded:"
+                "One or more paths not found in upstream caches and will be uploaded:"
             )
             for path in difference:
                 logging.info(f"* {path}")
@@ -190,15 +210,13 @@ def main(text_args):
             # 0. copy the full closure with zstd compression to a temporary
             #    directory
             # 1. check which .narinfo and .nar.zstd files correspond to the
-            #    missing cache in the directory, and add them to a "sync"
+            #    missing paths in the directory, and add them to a "sync"
             #    list.
             # 2. Invoke aws s3 sync. Because --size-only is passed, any paths
             #    already in the cache will not be uploaded.
             # ---
             # Potential improvements:
             #
-            # * Query cache to verify the paths don't already exist there
-            #   to save time on nix copy
             # * Only nix copy paths that we care about uploading -- blocked on
             #    https://github.com/NixOS/nix/issues/12835
             with tempfile.TemporaryDirectory(prefix="nix-eda-copy-uncache") as temp_dir:
@@ -244,6 +262,8 @@ def main(text_args):
                         "--include",
                         os.path.basename(narinfo_path),
                         "--include",
+                        # despite its name, thats just a relative path to the
+                        # .tar.zst
                         narinfo["URL"],
                     ]
                 if len(sync_include_args):
@@ -254,7 +274,7 @@ def main(text_args):
                             "s3",
                             "sync",
                             d,
-                            f"s3://{args_parsed.to_s3_bucket}",
+                            s3_nix_url._replace(query="").geturl(),
                             "--size-only",
                             "--exclude",
                             "*",
