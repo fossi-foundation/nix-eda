9dad4d7ceeb30ed5c6066216424d4e57a0fdffa4
Leo Moser <leomoser99@gmail.com>
2026-03-17 09:15:16 +0100

    chore: update magic to 8.3.623
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- nix/magic.nix
+++ nix/magic.nix
@@ -32,9 +32,9 @@
   cairo,
   python3,
   gnused,
-  version ? "8.3.619",
+  version ? "8.3.623",
   rev ? null,
-  sha256 ? "sha256-qLuXHBoGQxb5l82UYmXtnVs58UirKHLDnU4qC2eFnlw=",
+  sha256 ? "sha256-BgiNUS4U9xfmoIQC6Tn3ueR+zwU3hhOXYrKEhosQStw=",
 }:
 clangStdenv.mkDerivation {
   pname = "magic-vlsi";
