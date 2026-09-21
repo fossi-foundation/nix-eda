93b4808044ee1d92eb088f02e90bc9ae0b2bcf46
Leo Moser <leomoser99@gmail.com>
2026-09-21 14:15:32 +0200

    chore: update kepler-formal (#89)
    - kepler-formal -> `0ce1f2e` (2026-09-17)
      - added preFixup step to patch naja.so's rpath
      - added smoke-test installCheckPhase
    
    ---------
    
    Signed-off-by: Leo Moser <leomoser99@gmail.com>
    Co-authored-by: Mohamed Gaber <me@donn.website>

--- nix/kepler-formal.nix
+++ nix/kepler-formal.nix
@@ -5,8 +5,9 @@
 {
   lib,
   fetchGitHubSnapshot,
-  stdenv,
+  clangStdenv,
   python3,
+  bash,
   spdlog,
   zlib,
   boost,
@@ -18,12 +19,15 @@
   cmake,
   flex,
   bison,
+  perl,
+  autoPatchelfHook,
+  darwin,
   ctestCheckHook,
-  rev-date ? "2026-07-14",
-  rev ? "23ae50f5777f44d6a348264abd5f87f32d025330",
-  hash ? "sha256-tD9pbr/9iKejV3eTZcpvPAi7MnJd6K3AX0RTz94pBdI=",
+  rev-date ? "2026-09-17",
+  rev ? "0ce1f2ebd135d89cc3082725a9e38457f12d029c",
+  hash ? "sha256-EcQXgZt501vjoDw0BaTjC0dnB6GPtuIG36xJj4mrhwM=",
 }:
-stdenv.mkDerivation {
+clangStdenv.mkDerivation {
   pname = "kepler-formal";
   version = "0-unstable-${rev-date}";
 
@@ -35,6 +39,10 @@ stdenv.mkDerivation {
   };
 
   postPatch = ''
+    # This script generates some test scripts that harcode the shebang
+    substituteInPlace ./test/sec/RunSecStrategiesRegressTests.sh \
+      --replace-fail "#!/usr/bin/env bash" "#!${lib.getExe bash}"
+
     substituteInPlace ./thirdparty/naja/thirdparty/slang/external/CMakeLists.txt \
       --replace-fail "FIND_PACKAGE_ARGS 12.2" "FIND_PACKAGE_ARGS ${fmt.version}"
   '';
@@ -44,8 +52,11 @@ stdenv.mkDerivation {
     bison
     cmake
     pkg-config
+    perl
     ctestCheckHook
-  ];
+  ]
+  ++ lib.optionals clangStdenv.hostPlatform.isLinux [ autoPatchelfHook ]
+  ++ lib.optionals clangStdenv.hostPlatform.isDarwin [ darwin.autoSignDarwinBinariesHook ];
 
   buildInputs = [
     boost
@@ -64,9 +75,31 @@ stdenv.mkDerivation {
 
   doCheck = true;
 
+  preFixup =
+    # $out/bin/naja.so has invalid rpaths from the build process
+    lib.optionalString clangStdenv.hostPlatform.isLinux ''
+      patchelf --remove-rpath $out/bin/naja.so
+      patchelf --set-rpath $out/lib $out/bin/naja.so
+    ''
+    + lib.optionalString clangStdenv.hostPlatform.isDarwin ''
+      otool -l $out/bin/naja.so | perl -lne '$i = 3 if /LC_RPATH/; $i--; print /path (\S+)/ if $i == 0;' | xargs -n1 install_name_tool $out/bin/naja.so -delete_rpath
+      install_name_tool -add_rpath $out/lib $out/bin/naja.so
+    '';
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    runHook preInstallCheckPhase
+    echo "module mod1(input a, output b); assign b = ~a; endmodule" > ./mod_1.v
+    echo "module mod2(input a, output b); assign b = a ^ 1'b1; endmodule" > ./mod_2.v
+    $out/bin/kepler-formal -v sec -sv --design1 mod_1.v --design2 mod_2.v
+    runHook postInstallCheckPhase
+  '';
+
   meta = {
-    description = "equivalence checking tool for digital designs";
-    license = [ lib.licenses.mit ];
+    description = "Formal equivalence checking tool for digital circuits";
+    license = [ lib.licenses.asl20 ];
+    mainProgram = "kepler-formal";
     platforms = lib.platforms.all;
   };
 }
